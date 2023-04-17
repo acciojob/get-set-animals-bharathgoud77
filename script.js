932c86bb5848c12037e91065d725bed4248cfab3
bharathgoud77 <92040880+bharathgoud77@users.noreply.github.com>
2023-04-17 14:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this._species=species;
+	constructor(species) {
+		this.species = species;
 	}
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		console.log("the "+this._species+" makes sound.")
-	}
-}
 
-class Cat extends Animal {
-	purr(){
-		console.log("purr");
+	set setSpecies(species) {
+		this.species = species;
+	}
+
+	get getSpecies() {
+		return this.species;
+	}
+	
+	makeSound() {
+		console.log(`The ${this.species} makes a sound`);
 	}
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof");
+	constructor(species) {
+		super(species);
+		this.species = species;
+	}
+	bark() {
+		console.log(`woof`);
+	}
+}
+
+class Cat extends Animal {
+	constructor(species) {
+		super(species);
+		this.species = species;
+	}
+	purr() {
+		console.log(`purr`);
 	}
 }
 
